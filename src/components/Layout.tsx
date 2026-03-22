edaf66d8ed42d5936b1a947ef983e9d853c6743e
CobraKing777 <160332576+CobraKing777@users.noreply.github.com>
2026-03-22 19:08:38 +0700

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -83,7 +83,7 @@ export const Layout: React.FC<LayoutProps> = ({ children, currentView, setView }
       <div className="flex flex-col items-center px-2 mb-4">
         <div className="w-14 h-14 rounded-2xl overflow-hidden shadow-lg shadow-blue-500/10 border border-slate-800/50 hover:scale-105 transition-transform duration-500 bg-slate-900">
           <img 
-            src="https://drive.google.com/uc?export=view&id=1mz85BOIBtvDuJQmvUCntV5XrgbVokcrg" 
+            src="https://drive.google.com/uc?id=1mz85BOIBtvDuJQmvUCntV5XrgbVokcrg&export=view" 
             alt="RTFT" 
             className="w-full h-full object-cover"
             referrerPolicy="no-referrer"
